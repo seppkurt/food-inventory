eb120138bef3e150d5b3d458a92985c4e5c78309
seppkurt <sebastian.kurt@gmail.com>
2023-04-16 22:47:42 +0200

    some db preps

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,8 +1,15 @@
-import { supabase } from "$lib/supabaseClient";
+import { supabase } from '$lib/supabaseClient';
 
 export async function load() {
-  const { data } = await supabase.from("items").select();
-  return {
-    items: data ?? [],
-  };
+	const { data } = await supabase.from('items').select();
+	return {
+		items: data ?? []
+	};
 }
+/*
+export const actions = {
+	default: async ({ request }) => {
+		const data = await request.formData();
+		db.createItem(data.get('amount'), data.get('name'), data.get('strage_place'), data.get('due_date'));
+	}
+};*/
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,3 +9,25 @@
       <li>{item.amount} {item.name} in {item.storage_place} due {item.due_date}</li>
     {/each}
   </ul>
+<!--
+	<form method="POST">
+		<label>
+			Was neues? :
+			<input
+				name="amount"
+				autocomplete="off"
+			/>
+			<input
+				name="name"
+				autocomplete="off"
+			/>
+			<input
+				name="storage_place"
+				autocomplete="off"
+			/>
+			<input
+				name="due_date"
+				autocomplete="off"
+			/>
+		</label>
+	</form>-->
